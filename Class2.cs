ec5fdb428e34f93907ace4f797d5196320532cac
alexkaasik <aleks.kaasik@outlook.com>
2024-05-02 10:56:26 +0300

    Add project files.

---
+++ Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+
+namespace Kangelane
+{
+    public class Kangelane
+    {
+        /*
+        string nimi;
+        string asukoht;
+
+        Kangelane(string nimi, string asukoht) { 
+            Nimi = nimi;
+            Asukoht=asukoht;
+        }
+        
+        private string Nimi { get; set; }
+        private string Asukoht { get; set; }
+        */
+        public static string saved(int number) {
+
+            return (Math.Round(number * 0.95).ToString() + " olevate inimeste arvust");
+
+        }
+    }
+    public class SuperKangelane : Kangelane
+    {
+        public SuperKangelane() { }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Kangelane
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine(Kangelane.saved());
+            Console.WriteLine(Kangelane.saved(15));
+        }
+    }
+}
