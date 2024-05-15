d15aaa13e238930ed6f4225cb02c509fda9587af
alexkaasik <aleks.kaasik@outlook.com>
2024-05-15 07:32:53 +0300

    added heroes from a file, edit date of heroes, print

--- Function.cs
+++ Function.cs
@@ -32,33 +32,115 @@ namespace Kangelane
         public static void StatesFile(string HeroFile, Kangelane[] KangelaneK, SuperKangelane[] SuperKangelaneS)
         {
             string[] readText = File.ReadAllText(HeroFile).Split("\n");  // Read the contents of the file
-            int i = 0;
+            int i1 = 0;
+            int i2 = 0;
+            string name;
+            string location;
+            double spe;
+            //Console.WriteLine(KangelaneK.Length);
+
+            int b1 = KangelaneK.Count(x => x != null);
+            int b2 = SuperKangelaneS.Count(x => x != null);
+
             foreach (string line in readText){
 
-                string name = line.Split(",")[0].Trim();
-                string location = line.Split(",")[1].Trim();
-            
+                name = line.Split(",")[0].Trim();
+                location = line.Split(",")[1].Trim();
+                
                 //Nimi = name;
                 //Asukoht = location;
                 //if (name.Contains("*")){}
-                Console.WriteLine(KangelaneK[0].Nimi);
-                Console.WriteLine(SuperKangelaneS[0].Nimi);
-                
+            
+
                 if (name.Contains("*")){
-                    double spe = Convert.ToDouble(line.Split(",")[2].Trim());
-                    //Console.Write(name.Replace("*","") + ":" + location + ":" + spe);
-                    SuperKangelaneS[3+i].Nimi = name;
-                    SuperKangelaneS[3+i].Asukoht = location;
-                    SuperKangelaneS[3+i].Osavuse = spe;
+                    spe = System.Convert.ToDouble(line.Split(",")[2].Trim());
+
+                    //Console.WriteLine(name.Replace("*","") + ":" + location + ":" + spe);
+                    //Console.WriteLine(spe.GetType());
+                    SuperKangelaneS[b2+i1] = new SuperKangelane(name.Replace("*",""), location, spe);
+                    i1++;
                 }
                 /**/
                 else{
-                    //Console.Write(name.Replace("*","") + ":" + location);
-                    KangelaneK[3+i].Nimi = name;
-                    KangelaneK[3+i].Asukoht = location;
+                    //Console.Write(name + ":" + location);
+                    KangelaneK[b1+i2] = new Kangelane(name, location);
+                    i2++;
+
                 }
                 Console.WriteLine();
                 Function.print(KangelaneK, SuperKangelaneS);
+                
+            }
+        }
+        public static void edit(Kangelane[] KangelaneK, SuperKangelane[] SuperKangelaneS) {
+            string who;
+            do {
+                Console.Write("Who:");
+                who = Console.ReadLine();
+            } while (who.Length == 0);
+
+            if (who == "sup"){
+                for (int i = 0; i < SuperKangelaneS.Length && SuperKangelaneS[i] != null; i++){
+                    Console.WriteLine( i+1 + " " + SuperKangelaneS[i].ToString());
+                }
+            }
+            else {
+                for (int i = 0; i < KangelaneK.Length && KangelaneK[i] != null; i++){
+                    Console.WriteLine( i+1 + " " + KangelaneK[i].ToString());
+                }
+            }
+            int liner;
+            while (true) {
+                try{
+                    Console.Write("Line:");
+                    string line = Console.ReadLine();
+                    liner=int.Parse(line)-1;
+                    break;
+                }
+                catch {continue;}
+            }
+
+            
+
+            Console.Write("mode:");
+            string mode = Console.ReadLine();
+            string val;
+            switch (mode)
+            {
+                case "name":
+                    Console.Write("Edit name:");
+                    val = Console.ReadLine();
+                    if (who == "sup") {
+                        SuperKangelaneS[liner].Nimi = val;
+                    }
+                    else {
+                        KangelaneK[liner].Nimi = val;
+                    }
+                    break;
+                case "loca":
+                    Console.Write("Edit loca:");
+                    val = Console.ReadLine();
+                    if (who == "sup") {
+                        SuperKangelaneS[liner].Asukoht = val;
+                    }
+                    else {
+                        KangelaneK[liner].Asukoht = val;
+                    }
+                    break;
+                case "poin":
+                    if (who == "sup"){
+                        while (true) {
+                            try{
+                                Console.Write("Edit poin:");
+                                val = Console.ReadLine();
+                                double val2 = System.Convert.ToDouble(val);
+                                SuperKangelaneS[2].Osavuse = System.Convert.ToDouble(val2);
+                                break;
+                            }
+                            catch {continue;}
+                        }
+                    }
+                    break;
             }
         }
     }
